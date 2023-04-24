1bc4a8c7322d9bb635aa47e5e2529c2b2bb93229
lucasasrc <lucasasrc@gmail.com>
2023-04-24 19:52:23 -0300

    Adding Login Frontend

---
+++ Aplicação/React/src/component/Start.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import '../css/Start.css';
+import logo from '../img/logo.png';
+import { useNavigate } from 'react-router-dom';
+
+function Start() {
+    const navigate = useNavigate();
+
+    const entrarClick = () => {
+      navigate('/component/Login.js');
+    };
+
+    return (
+      <div className="Start">
+        <header className="Start-header">
+          <img src={logo} className="rentfyLogo" alt="logo" />
+          <p className="welcome">
+            Bem-Vindo ao Rentfy.
+          </p>
+          <p className="welcome">
+            Entre ou cadastre-se com as opções abaixo:
+          </p>
+  
+          <div>
+            <button onClick={entrarClick} className="btnEntrar">Entrar</button>
+            <button className="btnCadastrar">Cadastrar</button>
+          </div>
+        </header>
+      </div>
+    );
+}
+
+export default Start;
