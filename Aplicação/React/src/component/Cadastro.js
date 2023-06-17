826a8af7820eb3f6f6fafd1ef2a41054c97663d5
lucasasrc <lucasasrc@gmail.com>
2023-06-17 12:42:01 -0300

    Ajuste Login
    Inputs de locador e locatário.

--- Aplicação/React/src/component/Cadastro.js
+++ Aplicação/React/src/component/Cadastro.js
@@ -4,28 +4,38 @@ import logo from '../img/logo.png';
 
 function Cadastro() {
     return (
-      <div className="Cadastro">
-        <header className="Cadastro-header">
-            <form class="CadastroForm">
-                <img src={logo} className="rentfyLogoCadastro" alt="logo" />
-                <h3>Cadastre-se</h3>
+        <div className="Cadastro">
+            <header className="Cadastro-header">
+                <form className="CadastroForm">
+                    <img src={logo} className="rentfyLogoCadastro" alt="logo" />
+                    <h3>Cadastre-se</h3>
 
-                <label for="username" class="labelCadastro">Usuário</label>
-                <input type="text" placeholder="Usuário" id="username" class="inputCadastro"/>
+                    <label htmlFor="username" className="labelCadastro">Usuário</label>
+                    <input type="text" placeholder="Usuário" id="username" className="inputCadastro" />
 
-                <label for="password" class="labelCadastro">Senha</label>
-                <input type="password" placeholder="Senha" id="password" class="inputCadastro"/>
+                    <label htmlFor="password" className="labelCadastro">Senha</label>
+                    <input type="password" placeholder="Senha" id="password" className="inputCadastro" />
 
-                <label for="password" class="labelCadastro">Repita a Senha</label>
-                <input type="password" placeholder="Repita a Senha" id="repPassword" class="inputCadastro"/>
+                    <label htmlFor="password" className="labelCadastro">Repita a Senha</label>
+                    <input type="password" placeholder="Repita a Senha" id="repPassword" className="inputCadastro" />
 
-                <label for="password" class="labelCadastro">E-Mail</label>
-                <input type="email" placeholder="E-mail" id="email" class="inputCadastro"/>
+                    <label htmlFor="email" className="labelCadastro">E-Mail</label>
+                    <input type="email" placeholder="E-mail" id="email" className="inputCadastro" />
 
-                <button>Cadastrar</button>
-            </form>
-        </header>
-      </div>
+                    <label className="labelCadastro">Tipo de conta</label>
+                    <div className="radioContainer">
+                        <input type="radio" id="locador" name="conta" value="locador" />
+                        <label htmlFor="locador" className="radioFont">Locador</label>
+                    </div>
+                    <div className="radioContainer">
+                        <input type="radio" id="locatario" name="conta" value="locatario" />
+                        <label htmlFor="locatario" className="radioFont">Locatário</label>
+                    </div>
+
+                    <button>Cadastrar</button>
+                </form>
+            </header>
+        </div>
     );
 }
 
